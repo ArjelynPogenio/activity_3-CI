042a8dc72c378656869bb5273077f861761f9513
Lynnio <lynlyn143carlo@gmail.com>
2025-04-03 21:48:12 +0800

    Add CI pipeline for Python Testing

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 def multiply(a, b):
-    return a*b
+    return a * b
 
 def test_multiply():
-    assert multiply(2, 3) ==6
-    assert multiply(5, 0) ==0
+    assert multiply(2, 3) == 6
+    assert multiply(5, 0) == 0
     print("Test passed")
 
-if __name__ == "_main_":
-    test__multiply()
+if __name__ == "__main__":
+    test_multiply()
+
 
