35e07252b3a5ade94782a11b5ae50b1a8ac1ce8d
Lynnio <lynlyn143carlo@gmail.com>
2025-04-03 21:24:55 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def multiply(a, b):
+    return a*b
+
+def test_multiply():
+    assert multiply(2, 3) ==6
+    assert multiply(5, 0) ==0
+    print("Test passed")
+
+if __name__ == "_main_":
+    test__multiply()
+
